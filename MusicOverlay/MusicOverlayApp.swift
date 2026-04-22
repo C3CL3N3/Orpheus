e132cd9744c4065f0416e9be3851b96f44d7476e
Enzo Curci <enzocurci2005@gmail.com>
2026-04-22 21:19:02 +0800

    Initial Commit

---
+++ MusicOverlay/MusicOverlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicOverlayApp.swift
+//  MusicOverlay
+//
+//  Created by Enzo Curci on 22/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct MusicOverlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
